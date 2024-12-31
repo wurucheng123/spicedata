a2006538308032384a19411b33e9a437314663f5
wurucheng <wrc1953@outlook.com>
2024-12-31 16:54:39 +0800

    add function definition

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -2,7 +2,7 @@
 
 library(tidytuesdayR)
 
-tuesdata = tt_load('2021-12-14')
-studio_album_tracks = tuesdata$studio_album_tracks
+tuesdata <- tt_load("2021-12-14")
+studio_album_tracks <- tuesdata$studio_album_tracks
 
 usethis::use_data(studio_album_tracks, overwrite = TRUE)
