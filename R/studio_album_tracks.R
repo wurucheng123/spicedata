3e66189756417167cb0735c9998221c6eb14d9d1
wurucheng <wrc1953@outlook.com>
2024-12-31 15:50:27 +0800

    document the dataset

---
+++ R/studio_album_tracks.R
@@ -0,0 +1,11 @@
+#' Spice data
+#'
+#' Shrimp are classified by size, 0-15 shrimp per pound, 15-20 shrimp per pound, etc. A smaller number per pound indicates larger shrimp. Nominal prices are total monthly value of brown shrimp andings within size class divided by total monthly landings within the size class.
+#'
+#' @format A tibble with 31 rows and 25 variables:
+#' \describe{
+#'   \item{artist_name}{chr Name}
+#'   \item{artist_id}{chr Month price was recorded. Ranges from 1-12 for January - December}
+#' }
+#' @source <https://github.com/rfordatascience/tidytuesday/tree/master/data/2021/2021-12-14>
+"studio_album_tracks"
