80d2ed0f8999a0251a2854ee32399c250bab469e
wurucheng <wrc1953@outlook.com>
2024-12-31 15:17:38 +0800

    add data to pkg

---
+++ data-raw/DATASET.R
@@ -0,0 +1,8 @@
+## code to prepare `DATASET` dataset goes here
+
+library(tidytuesdayR)
+
+tuesdata = tt_load('2021-12-14')
+studio_album_tracks = tuesdata$studio_album_tracks
+
+usethis::use_data(studio_album_tracks, overwrite = TRUE)
